81d0a5431142df9312d7d4406abab605b9a79860
chaitnya23 <chaitnyagiri2306@gmail.com>
2023-05-31 22:17:52 +0530

    added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,8 +5,8 @@ import LoginDecorator from "../components/LoginDecorator";
 
 export default function Login() {
   return (
-    <div className="flex w-full md:gap-28 p-8">
-      <div className="w-[50%]">
+    <div className="flex w-full md:gap-28 gap-12 p-8">
+      <div className="md:w-[50%] w-[40%]">
         <LoginDecorator/>
       </div>
       <div className="md:w-[30%] w-[45%]">
